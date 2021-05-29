8db80f095127edb07b63961231ec2ffb6f01db33
Cas School <c.bukkems@student.fontys.nl>
2021-05-29 14:16:12 +0200

    First commit CMR POC

---
+++ header.php
@@ -0,0 +1,14 @@
+<!doctype html>
+
+
+<head>
+    <html lang="nl">
+    <meta charset="utf-8">
+    <title>Centrale machtigingen systeem</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5.0, minimum-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" /> 
+    <meta name="description" content="Het centrale machtigings plattform van Nederland"/>
+    <link rel="stylesheet" href="css/style.min.css">
+</head>
+
+<body>
---
+++ index.php
@@ -0,0 +1,42 @@
+    <?php include 'header.php'; ?>
+
+    <header class="banner_top" role="banner">
+        <a href="index.html">
+            <img class="banner_top_logo" src="img/cmr_logo.svg" alt="Centraal Machtigingen Register logo" />
+        </a>
+    </header>
+    <nav aria-label="Hoofdmenu">
+        <div class="container">
+            <ul class="menubar_navigation" role="menubar" aria-label="Centraal Machtigingen Register">
+                <li class="active" role="none">
+                    <a role="menuitem" href="#home">
+                        Home
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <main id="main">
+        <div class="top_img_header" aria-label="Twee personen samen aan het machtigen" title="Twee personen samen aan het machtigen" style="background-image: url('img/pexels-marcus-aurelius-6787960.jpg')">
+            <div class="container">
+                <div class="top_img_text">
+                    <h1>Al je machtigingen op een centrale plek</h1>
+                </div>
+            </div>
+        </div>
+        <div class="intro_text">
+            <div class="container"> 
+                <div class="intro_text_wrap">
+                    <h2>Wat is Centraal Machtigen?</h2>
+                    <p>Met het Centrale Machtigingen Register kunt u geholpen worden of iemand helpen met het regelen van zaken met de overheid. Door iemand te 'machtigen' of 'gemachtigd' te worden kan dit veilig, zonder uw DigiD af te geven.</p>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <footer>
+
+
+    </footer>
+
+</body>
